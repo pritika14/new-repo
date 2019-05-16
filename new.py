c28256a6239ba3928a0c4164a6597c3206a3dac7
pritika Saran <pritika.saran@globallogic.com>
2019-05-16 17:16:21 +0530

    ver3

--- new.py
+++ new.py
@@ -1,2 +1,15 @@
-print("no")
-print("yes")
+a = [[1,2,3],[2,3,4]]
+b = []
+i =0
+j=0
+while(i<len(a)):
+	j = 0
+	while(j<len(a[i])):
+		b.append(a[i][j])
+		j = j + 1
+	i = i + 1
+b.sort()
+med = len(b)/2
+med = int(med)
+print(b[med])
+
